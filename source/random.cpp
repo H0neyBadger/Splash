e681d8fbcfe3434c77554aa6f4c3da122166d540
h0neybadger <h0neybadger.git@gmail.com>
2022-01-01 18:05:33 +0100

    Fix null pokemons in random list

--- source/random.cpp
+++ source/random.cpp
@@ -2,20 +2,24 @@
 #include "splash/random.hpp"
 
 void Random::ShuffleList(uint16_t* lst, uint32_t len) {
-    int32_t j, seed = this->seed;
     uint16_t swap;
+    uint32_t j, seed = this->seed;
     uint64_t r;
 
     if (seed == 0) {
         throw "Invalid seed";
     }
 
-    for (uint32_t i = len; i > 1; i--) {
+    for (uint32_t i = len - 1; i > 0; i--) {
         // naive attempt to create randomness from seed
         // this is not a real random
         r = i * (uint64_t)seed;
-        seed ^= r & 0xffffffff;
-        seed ^= r >> 32 & 0xffffffff;
+        if (r != seed) {
+            seed ^= r & 0xffffffff;
+        }
+        if (r >> 32 != seed) {
+            seed ^= (r >> 32) & 0xffffffff;
+        }
 
         // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
         j = seed % i;  // random in range
